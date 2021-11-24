5f0ad35b42898fa29d62649124f057c6250996a0
fraol123 <fraoltesfaye.ft@gmail.com>
2021-11-24 17:27:41 +0000

    linted

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then(() => {
-	console.log('Got a response from the APi');
-    }, (err) => err);
+  promise.then(() => {
+    console.log('Got a response from the APi');
+  }, (err) => err);
 }

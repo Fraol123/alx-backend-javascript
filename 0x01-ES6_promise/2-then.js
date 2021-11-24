12c11f2fad34ea73998beeb465b5270b7dcfbc43
fraol123 <fraoltesfaye.ft@gmail.com>
2021-11-24 19:17:57 +0000

    new

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
-    console.log('Got a response from the APi');
+    console.log('Got a response from the API');
   }, (err) => err);
 }

4d86ce17939d412a0038b4d7a45f0cc65578d2c6
fraol123 <fraoltesfaye.ft@gmail.com>
2021-11-24 19:12:52 +0000

    try

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+    queue.push('Guardrail was processed');
+  } catch (err) {
+    queue.push(err.toString());
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
